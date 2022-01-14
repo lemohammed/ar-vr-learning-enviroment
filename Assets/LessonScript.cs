03cfcc4266b073b06fa0c9cb9ff52337be59ffc3
Mohamad Abdel Rida <mohamad.abdelrida@ucaglary.ca>
2022-01-14 04:06:06 -0700

    chore: upgraded unity

---
+++ Assets/LessonScript.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro; 
+
+public class LessonScript : MonoBehaviour
+{
+    public TMP_Text textMesh;
+    // Start is called before the first frame update
+    void Start()
+    {
+          // Set text
+    textMesh.text = "DETERMINATION";
+
+   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
